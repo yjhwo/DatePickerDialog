4dcf353dcf0092e87607e905a2e502898aa35c0a
yjhwo <yjhwo@naver.com>
2016-05-26 15:07:41 +0900

    0526_TimePickerDialog완성

--- app/src/main/java/com/estsoft/datepickerdialog/MainActivity.java
+++ app/src/main/java/com/estsoft/datepickerdialog/MainActivity.java
@@ -1,6 +1,7 @@
 package com.estsoft.datepickerdialog;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,6 +9,8 @@ import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import java.util.Calendar;
 
@@ -31,5 +34,17 @@ public class MainActivity extends AppCompatActivity {
         },calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH)+1, calendar.get(Calendar.DAY_OF_MONTH) );
 
         dpDialog.show();
+    }
+
+    public void dialogTimePicker(View view){
+
+        TimePickerDialog tpDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                Toast.makeText(getApplicationContext(), hourOfDay + "시 " + minute + "분", Toast.LENGTH_SHORT).show();
+            }
+        },15,24,true);
+
+        tpDialog.show();
     }
 }
